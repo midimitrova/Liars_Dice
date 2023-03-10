3204e10b41b0ea0a591656d29de6027391912ae8
midimitrova <mariyapopova95@gmail.com>
2023-03-10 17:32:43 +0200

    rewrite try/except for user input for number of computer players

--- game.py
+++ game.py
@@ -9,22 +9,28 @@ class Game:
         self.list_of_players = []
 
     def add_players(self):
-        computer_players = 0
-
         while True:
             try:
                 user_input = HumanPlayer(input("Enter your name: ").strip())
                 self.list_of_players.append(user_input)
                 break
+
             except ValueError:
                 print('Your name should contain only letters!')
 
-        while computer_players < 1 or computer_players > 10:
+        while True:
             try:
                 computer_players = int(input("Enter a number between 1 and 10 to choose "
                                              "how many computer players to have: "))
+
             except ValueError:
-                print('Please enter a valid number!')
+                print('Please enter a digit!')
+
+            else:
+                if 1 <= computer_players <= 10:
+                    break
+                else:
+                    print("Out of range. Try again!")
 
         while computer_players > 0:
             random_computer_player = ComputerPlayer(choice(ComputerPlayer.computer_name_list))
