d6d0b165da371ee4c7216404618f062e598c7e94
midimitrova <mariyapopova95@gmail.com>
2023-03-10 08:12:41 +0200

    Make Player Abstract class, HumanPlayer and ComputerPlayer inherit Player

--- player.py
+++ player.py
@@ -1,28 +1,38 @@
+from abc import ABC, abstractmethod
 from random import randint
 
 
-class Player:
+class Player(ABC):
+    TOTAL_DICE_COUNT = 0
 
     def __init__(self, name):
         self.name = name
         self.num_of_dice = 5
         self.player_dice = []
         self.roll_dice()
-
-    @property
-    def name(self):
-        return self.__name
-
-    @name.setter
-    def name(self, value):
-        if not value.isalpha():
-            raise ValueError('Your name should contain only letters!')
-
-        self.__name = value.title()
+        self.total_dice = Player.TOTAL_DICE_COUNT
 
     def roll_dice(self):
-        player_dice = []
+        player_roll = []
         for _ in range(self.num_of_dice):
             roll = randint(1, 6)
-            player_dice.append(roll)
-        return player_dice
+            player_roll.append(roll)
+        self.player_dice = player_roll
+        return self.player_dice
+
+    def count_total_dice(self):
+        result = Player.TOTAL_DICE_COUNT
+        Player.TOTAL_DICE_COUNT += self.num_of_dice
+        return result
+
+    def remove_die(self):
+        if len(self.player_dice) > 0:
+            self.player_dice.pop()
+            self.num_of_dice -= 1
+            Player.TOTAL_DICE_COUNT -= 1
+
+
+
+
+
+
