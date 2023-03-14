1da4ff4b48162943b67f46ba011dbade692d9b9b
midimitrova <mariyapopova95@gmail.com>
2023-03-14 08:45:05 +0200

    move activating_wild_ones method, write methods for getting players and methods for wild "ones" mode

--- game.py
+++ game.py
@@ -9,7 +9,9 @@ class Game:
 
     def __init__(self):
         self.list_of_players = []
-        self.bet = {"dice_count": 3, "dice_value": 2}
+        self.bet = {"dice_count": 0, "dice_value": 0}
+        self.current_player = ''
+        self.previous_player = ''
 
     def add_players(self):
         while True:
@@ -39,16 +41,58 @@ class Game:
             else:
                 continue
 
-    def turn_on_additional_rules(self):
+    @staticmethod
+    def activating_wild_ones():
+        print('Additional rule for advanced players: '
+              'The "ones" face of the dice is considered wild - it always counts as the face of the current bid.')
+
+        while True:
+            try:
+                user_input = input('Do you want to activate "wild ones" mode? '
+                                   'Choose "yes" or "no": ').strip().lower()
+
+                if user_input not in ['yes', 'no']:
+                    raise ValueError
+                break
+
+            except ValueError:
+                print('Please enter "yes" or "no"!')
+
+        return user_input
+
+    @staticmethod
+    def check_is_player_human(player):
+        if player.__class__.__name__ == 'HumanPlayer':
+            return True
+        return False
+
+    def turn_on_additional_rules(self, player):
         result = ''
-        for current_player in self.list_of_players:
-            if current_player.__class__.__name__ == 'HumanPlayer':
-                result = current_player.activating_wild_ones()
+        if self.check_is_player_human(player):
+            result = self.activating_wild_ones()
+
         return result
 
-    # def choose_dice_combination(self):
-    #     choose from dice combination if make_decision is bid, and with or no wild ones
+    def choose_starting_player(self):
+        if self.current_player == '':
+            self.current_player = choice(self.list_of_players)
+            if self.check_is_player_human(self.current_player):
+                self.turn_on_additional_rules(self.current_player)
 
+    def choose_next_player(self):
+        current_player_index = self.list_of_players.index(self.current_player)
+        next_player_index = (current_player_index + 1) % len(self.list_of_players)
+        next_player = self.list_of_players[next_player_index]
 
+        self.previous_player = self.current_player
+        self.current_player = next_player
 
+    def check_liar_mode(self):
+        pass
 
+    def collect_wild_ones(self):
+        collect_all_dice = self.bet["dice_count"]
+        if self.turn_on_additional_rules == 'yes':
+            collect_all_dice += Player.PLAYERS_DICE_VALUES[1]
+
+        return collect_all_dice
