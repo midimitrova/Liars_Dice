ece087a9107540dbd6bc09ba8793ff4fead5545e
midimitrova <mariyapopova95@gmail.com>
2023-03-15 14:58:48 +0200

    rewrite make_bet to throw ValueError when new user bet is not valid, rewrite make_decision to throw ValueError when there are no more valid combinations

--- human_player.py
+++ human_player.py
@@ -19,40 +19,40 @@ class HumanPlayer(Player):
 
     def make_decision(self, bet):
         while True:
+            try:
+                user_input = input('\nWould you like to make a higher bid or calling last bidder a liar? '
+                                   'Choose "b" for bid or "l" for liar: ').strip().lower()
 
-            user_input = input('\nWould you like to make a higher bid or calling last bidder a liar? '
-                               'Choose "b" for bid or "l" for liar: ').strip().lower()
+                if user_input == 'b' and self.choose_valid_dice_combination(bet) is None:
+                    raise ValueError
 
-            if user_input == 'b':
-                return 'bid'
-            elif user_input == 'l':
-                return 'liar'
+            except ValueError:
+                print("There aro no more valid combinations to choose. Please enter 'l' for liar.")
+                continue
+
+            else:
+                if user_input == 'b':
+                    return 'bid'
+                elif user_input == 'l':
+                    return 'liar'
 
     def make_bet(self, bet):
         new_user_bet = {'dice_count': 0, 'dice_value': 0}
-
-        to_print_combinations = []
-        print('\nYou can choose from these combinations: ')
-        valid_combinations = self.choose_valid_dice_combination(bet)
-        for comb in valid_combinations:
-            dice_count, dice_value = comb
-            to_print_combinations.append(f'{dice_count} X {dice_value}')
-        print(', '.join(to_print_combinations))
-        print('Place your bet.')
 
         while True:
             try:
                 new_user_bet['dice_count'] = int(input('Please, enter number of dice: '))
                 new_user_bet['dice_value'] = int(input('Please, enter face value of dice: '))
 
+                if not self.check_bet_is_valid(new_user_bet['dice_count'], new_user_bet['dice_value'], bet):
+                    raise ValueError
+
             except ValueError:
-                print('Please, enter a digit')
+                print("You should bid a higher quantity of the same face or any particular quantity of a higher face.\n"
+                      f"You can't bid a number of dice larger than all dice on table: {Player.TOTAL_DICE_COUNT}, "
+                      f"or a face value of die lower than 1 and higher than 6.")
                 continue
 
             else:
                 if self.check_bet_is_valid(new_user_bet['dice_count'], new_user_bet['dice_value'], bet):
-                    # print(f"{player.name}'s bid is: {new_user_bet['dice_count']} X {new_user_bet['dice_value']}")
                     return new_user_bet
-
-
-
